25e364605d2b50d9757e4ef183e4af1b93016465
PYTH2 <109158659+PYTH2@users.noreply.github.com>
2022-07-12 18:04:36 +0100

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("lol").cek_pw()
+       __import__("lol").kontol()
    except Exception as e:
        exit(str(e))

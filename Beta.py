624bdf367bf3df8f7fb13610a17eca49996cf48e
PYTH2 <109158659+PYTH2@users.noreply.github.com>
2022-07-13 18:20:54 +0100

    Update Beta.py
    git clone https://github.com/PYTH2/last.git
    
    cd last
    
    python Beta.py

--- Beta.py
+++ Beta.py
@@ -6,11 +6,11 @@ except:
  
 import requests
 bit = platform.architecture()[0]
-if bit == '32bit':
+if bit == '64bit':
     print("\x1b[1;92mCongratulations your mobile Support This Tools")
     from OK import login
     login()
-elif bit == '64bit':
+elif bit == '32bit':
     os.system('xdg-open https://wa.me/+2347013107449')
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
  
